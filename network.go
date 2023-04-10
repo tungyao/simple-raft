eac466ac53be78ad078a49d7a3fd5175cb0f9c91
tungyao <1922527051@qq.com>
2023-04-10 17:15:19 +0800

    需要修改网络协议，考虑是不是要修改allNode的结构，来兼容更多的东西

--- network.go
+++ network.go
@@ -1,8 +1,9 @@
 package simple_raft
 
 import (
-	"log"
+	"fmt"
 	"net"
+	"os"
 	"unsafe"
 )
 
@@ -15,6 +16,7 @@ type network struct {
 	Address string
 	Rece    chan []byte
 	Send    chan []byte
+	Conn    net.Conn
 }
 
 // In fact , there's a node list in program
@@ -29,62 +31,99 @@ type network struct {
 //
 
 func (ne *network) Run() {
+
+	// TODO 网络功能需要重新设计
 	ne.Rece = make(chan []byte, 512)
 	ne.Send = make(chan []byte, 512)
-	lis, err := net.Listen("tcp", ne.self.Addr)
+	ln, err := net.Listen("tcp", ne.self.Addr)
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
 		return
 	}
-	go func() {
-		for {
-			select {
-			case r := <-ne.Rece:
-				log.Println(r)
-			case s := <-ne.Send:
-				log.Println(s)
-			}
-		}
-	}()
+	defer ln.Close()
+
+	fmt.Println("Listening on :", ne.self.Addr)
+	connections := make(map[net.Conn]bool)
 	for {
-		conn, err := lis.Accept()
+		conn, err := ln.Accept()
 		if err != nil {
-			log.Println(err)
+			fmt.Println(err)
 			continue
 		}
-		go func(connInner net.Conn) {
-			go func() {
-				for {
-					data := <-ne.Send
-					connInner.Write(data)
-				}
-			}()
-			for {
-				var data = make([]byte, 1024)
-				n, _ := connInner.Read(data)
-				ne.Rece <- data[:n]
+		connections[conn] = true
+		go handleConnection(conn, connections)
 
-				// 加入集群
-				if data[2] == 0x2 {
-					nd := &Node{
-						Timeout: int(uint32(data[4]&0xff)<<8) + int(data[3]&0xff),
-					}
-					mux.Lock()
-					allNode = append(allNode, nd)
-					mux.Unlock()
-				}
+	}
+}
 
+func handleConnection(conn net.Conn, connections map[net.Conn]bool) {
+	// TODO 这里是写个狗屁
+	// 正确的需求是什么
+	// 节点都是以listen方式监听在某一个端口上，其他节点加入的时候，需要记录conn结构体，还需要同时发送和接受消息
+	// 同时接收和发送数据
+	var data [1024]byte
+	for {
+		// 接收数据
+		_, err := conn.Read(data[:])
+		if err != nil {
+			return
+		}
+		// TODO 这里需要一个数据汇总的东西
+
+		// 加入集群
+		if data[2] == 0x2 {
+			// 声明超时时间
+			nd := &Node{
+				Timeout: int(uint32(data[4]&0xff)<<8) + int(data[3]&0xff),
+				Net:     &network{Conn: conn},
 			}
+			mux.Lock()
+			allNode = append(allNode, nd)
+			mux.Unlock()
+		}
 
-		}(conn)
+	}
+
+}
+
+func sendData(conn net.Conn) {
+	for {
+		fmt.Print("Enter text: ")
+		var input string
+		fmt.Scanln(&input)
+
+		if input == "/quit" {
+			conn.Close()
+			os.Exit(0)
+		}
+
+		_, err := conn.Write([]byte(input))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+}
+
+func receiveData(conn net.Conn) {
+	buffer := make([]byte, 1024)
+
+	for {
+		size, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		data := string(buffer[:size])
+		fmt.Println("Received:", data)
 	}
 }
 
 // HeartRequest 向其他节点发送心跳 以超时事件最短的为发送时间
 func (ne *network) HeartRequest(nodes []*Node) {
 	// TODO send heart to each node
-	for _, _ = range allNode {
-		ne.self.Net.Send <- []byte{0, 0, 1}
+	for _, v := range allNode {
+		v.Net.Send <- []byte{0, 0, 1}
 	}
 }
 
